4d4bc04c3a26d758d0981aa2c14d0828e8f768fb
Siddhartha <sid@countvajhula.com>
2020-11-06 18:49:35 -0800

    superlative controls in char mode

--- eem-char-mode.el
+++ eem-char-mode.el
@@ -20,23 +20,33 @@
                     (+ (point) 1)
                     (quote exclusive) nil))
 
-(defun my-move-char-left ()
+(defun my-move-char-left (&optional superlative)
   "Move character left"
   (interactive)
   (when (not (bolp))
     (my-delete-char)
     (let ((at-eol (eolp)))
-      (evil-backward-char)
+      (cond ((eq superlative nil) (evil-backward-char))
+            ((eq superlative 'more) (evil-backward-char 3))
+            ((eq superlative 'most) (evil-beginning-of-line)))
       (if at-eol
+          ;; for some reason delete-char doesn't update point
+          ;; while in hydra at EOL, so the handling here
+          ;; is different than it otherwise would be
           (progn (evil-paste-after nil nil)
                  (backward-char))
         (evil-paste-before nil nil)))))
 
-(defun my-move-char-right ()
+(defun my-move-char-right (&optional superlative)
   "Move character right"
   (interactive)
   (when (not (eolp))
     (my-delete-char)
+    (cond ((eq superlative 'more)
+           (condition-case nil
+               (evil-forward-char 2)
+             (error nil)))
+          ((eq superlative 'most) (evil-end-of-line)))
     (evil-paste-after nil nil)
     ;; Note: The above is sufficient when this command is run
     ;; interactively via M-x. But when run via the hydra, it
@@ -45,47 +55,23 @@
     ;; adding the line below for usage via hydra.
     (backward-char)))
 
-(defun my-move-char-down ()
+(defun my-move-char-down (&optional superlative)
   "Move character down"
   (interactive)
   (my-delete-char)
-  (evil-next-line)
+  (cond ((eq superlative nil) (evil-next-line))
+        ((eq superlative 'more) (evil-next-line 3))
+        ((eq superlative 'most) (evil-forward-paragraph)))
   (evil-paste-before nil nil))
 
-(defun my-move-char-up ()
+(defun my-move-char-up (&optional superlative)
   "Move character up"
   (interactive)
   (my-delete-char)
-  (evil-previous-line)
+  (cond ((eq superlative nil) (evil-previous-line))
+        ((eq superlative 'more) (evil-previous-line 3))
+        ((eq superlative 'most) (evil-backward-paragraph)))
   (evil-paste-before nil nil))
-
-(defun my-move-char-far-left ()
-  "Move character far left"
-  (interactive)
-  (my-delete-char)
-  (evil-beginning-of-line)
-  (evil-paste-before nil nil))
-
-(defun my-move-char-very-bottom ()
-  "Move character to the bottom"
-  (interactive)
-  (my-delete-char)
-  (evil-goto-line)
-  (evil-paste-before nil nil))
-
-(defun my-move-char-very-top ()
-  "Move character to the top"
-  (interactive)
-  (my-delete-char)
-  (evil-goto-first-line)
-  (evil-paste-before nil nil))
-
-(defun my-move-char-far-right ()
-  "Move character far right"
-  (interactive)
-  (my-delete-char)
-  (evil-end-of-line)
-  (evil-paste-after nil nil))
 
 (defun my-change-char ()
   "Change character"
@@ -114,14 +100,44 @@
   ("j" evil-next-line "down")
   ("k" evil-previous-line "up")
   ("l" evil-forward-char "right")
+  ("C-h" (lambda ()
+           (interactive)
+           (evil-backward-char 3)) "more left")
+  ("C-j" (lambda ()
+           (interactive)
+           (evil-next-line 3)) "more down")
+  ("C-k" (lambda ()
+           (interactive)
+           (evil-previous-line 3)) "more up")
+  ("C-l" (lambda ()
+           (interactive)
+           (evil-forward-char 3)) "more right")
+  ("M-h" (lambda ()
+           (interactive)
+           (evil-beginning-of-line)) "most left")
+  ("M-j" (lambda ()
+           (interactive)
+           (evil-forward-paragraph)
+           (evil-previous-line)) "most down")
+  ("M-k" (lambda ()
+           (interactive)
+           (evil-backward-paragraph)
+           (evil-next-line)) "most up")
+  ("M-l" (lambda ()
+           (interactive)
+           (evil-end-of-line)) "most right")
   ("H" my-move-char-left "move left")
   ("J" my-move-char-down "move down")
   ("K" my-move-char-up "move up")
   ("L" my-move-char-right "move right")
-  ("M-H" my-move-char-far-left "move to far left")
-  ("M-J" my-move-char-very-bottom "move to bottom")
-  ("M-K" my-move-char-very-top "move to top")
-  ("M-L" my-move-char-far-right "move to far right")
+  ("C-S-h" (lambda () (interactive) (my-move-char-left 'more)) "move left more")
+  ("C-S-j" (lambda () (interactive) (my-move-char-down 'more)) "move down more")
+  ("C-S-k" (lambda () (interactive) (my-move-char-up 'more)) "move up more")
+  ("C-S-l" (lambda () (interactive) (my-move-char-right 'more)) "move right more")
+  ("M-H" (lambda () (interactive (my-move-char-left 'most))) "move to far left")
+  ("M-J" (lambda () (interactive) (my-move-char-down 'most)) "move to bottom")
+  ("M-K" (lambda () (interactive) (my-move-char-up 'most)) "move to top")
+  ("M-L" (lambda () (interactive) (my-move-char-right 'most)) "move to far right")
   ("c" my-change-char "change" :exit t)
   ("y" my-yank-char "yank (copy)" :exit t)
   ("x" my-delete-char "delete")
