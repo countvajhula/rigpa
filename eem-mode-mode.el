0390289abe1be2dbcebcd7497660952f29e6944c
Siddhartha <sid@countvajhula.com>
2020-11-14 14:54:12 -0800

    fix level mode entry to defer to lithium mode entry

--- eem-mode-mode.el
+++ eem-mode-mode.el
@@ -7,13 +7,12 @@
 (defun eem-enter-mode (mode-name)
   "Enter mode MODE-NAME."
   (message "entering mode %s" mode-name)
-  (let ((mode-entry (if (member mode-name (list "normal" "insert" "emacs"))
-                        ;; handle the (at present, hypothetical) case of entry
-                        ;; to a standard evil mode
-                        ;; no hydra for standard evil modes
-                        (intern (concat "evil-" mode-name "-state"))
-                      (intern (concat "hydra-" mode-name "/body")))))
-    (funcall mode-entry))
+  (if (member mode-name (list "normal" "insert" "emacs"))
+      ;; handle the (at present, hypothetical) case of entry
+      ;; to a standard evil mode
+      ;; no hydra for standard evil modes
+      (funcall (intern (concat "evil-" mode-name "-state")))
+    (lithium-enter-mode mode-name))
   (message "entered mode %s" mode-name))
 
 (defun eem--enter-level (level-number)
