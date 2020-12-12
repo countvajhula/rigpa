9ac0a883bf69217e89957efde9667eb541b4e903
Siddhartha <sid@countvajhula.com>
2020-12-12 10:43:39 -0800

    rename char mode interfaces

--- rigpa-char-mode.el
+++ rigpa-char-mode.el
@@ -10,23 +10,24 @@
   ;;:suppress-keymap) ;; should be t, but probably inherits from normal
   :enable (normal))
 
-(defun my-char-info ()
+(defun rigpa-char-info ()
   "Info on character"
   (interactive)
   (what-cursor-position))
 
-(defun my-delete-char ()
+(defun rigpa-char-delete ()
   "Delete character"
   (interactive)
   (evil-delete-char (point)
                     (+ (point) 1)
                     (quote exclusive) nil))
 
-(defun my-move-char-left (&optional superlative)
+;; TODO: seems to vary depending on the value of evil-move-cursor-back
+(defun rigpa-char-move-left (&optional superlative)
   "Move character left"
   (interactive)
   (when (not (bolp))
-    (my-delete-char)
+    (rigpa-char-delete)
     (let ((at-eol (eolp)))
       (cond ((eq superlative nil) (evil-backward-char))
             ((eq superlative 'more) (evil-backward-char 3))
@@ -41,11 +42,11 @@
                    (backward-char)))
         (evil-paste-before nil nil)))))
 
-(defun my-move-char-right (&optional superlative)
+(defun rigpa-char-move-right (&optional superlative)
   "Move character right"
   (interactive)
   (when (not (eolp))
-    (my-delete-char)
+    (rigpa-char-delete)
     (cond ((eq superlative 'more)
            (condition-case nil
                (evil-forward-char 2)
@@ -59,25 +60,25 @@
     ;; adding the line below for usage via hydra.
     (backward-char)))
 
-(defun my-move-char-down (&optional superlative)
+(defun rigpa-char-move-down (&optional superlative)
   "Move character down"
   (interactive)
-  (my-delete-char)
+  (rigpa-char-delete)
   (cond ((eq superlative nil) (evil-next-line))
         ((eq superlative 'more) (evil-next-line 3))
         ((eq superlative 'most) (evil-forward-paragraph)))
   (evil-paste-before nil nil))
 
-(defun my-move-char-up (&optional superlative)
+(defun rigpa-char-move-up (&optional superlative)
   "Move character up"
   (interactive)
-  (my-delete-char)
+  (rigpa-char-delete)
   (cond ((eq superlative nil) (evil-previous-line))
         ((eq superlative 'more) (evil-previous-line 3))
         ((eq superlative 'most) (evil-backward-paragraph)))
   (evil-paste-before nil nil))
 
-(defun my-change-char ()
+(defun rigpa-char-change ()
   "Change character"
   (interactive)
   (evil-substitute (point)
@@ -85,12 +86,12 @@
                    (quote exclusive)
                    nil))
 
-(defun my-yank-char ()
+(defun rigpa-char-yank ()
   "Yank (copy) character"
   (interactive)
   (evil-yank-characters (point) (+ (point) 1)))
 
-(defun my-toggle-case-char ()
+(defun rigpa-char-toggle-case ()
   "Toggle upper-/lower-case"
   (interactive)
   (evil-invert-char (point) (+ (point) 1) (quote exclusive)))
@@ -130,24 +131,24 @@
   ("M-l" (lambda ()
            (interactive)
            (evil-end-of-line)) "most right")
-  ("H" my-move-char-left "move left")
-  ("J" my-move-char-down "move down")
-  ("K" my-move-char-up "move up")
-  ("L" my-move-char-right "move right")
-  ("C-S-h" (lambda () (interactive) (my-move-char-left 'more)) "move left more")
-  ("C-S-j" (lambda () (interactive) (my-move-char-down 'more)) "move down more")
-  ("C-S-k" (lambda () (interactive) (my-move-char-up 'more)) "move up more")
-  ("C-S-l" (lambda () (interactive) (my-move-char-right 'more)) "move right more")
-  ("M-H" (lambda () (interactive (my-move-char-left 'most))) "move to far left")
-  ("M-J" (lambda () (interactive) (my-move-char-down 'most)) "move to bottom")
-  ("M-K" (lambda () (interactive) (my-move-char-up 'most)) "move to top")
-  ("M-L" (lambda () (interactive) (my-move-char-right 'most)) "move to far right")
-  ("c" my-change-char "change" :exit t)
-  ("y" my-yank-char "yank (copy)" :exit t)
-  ("x" my-delete-char "delete")
-  ("~" my-toggle-case-char "toggle case")
-  ("i" my-char-info "info" :exit t)
-  ("?" my-char-info "info" :exit t)
+  ("H" rigpa-char-move-left "move left")
+  ("J" rigpa-char-move-down "move down")
+  ("K" rigpa-char-move-up "move up")
+  ("L" rigpa-char-move-right "move right")
+  ("C-S-h" (lambda () (interactive) (rigpa-char-move-left 'more)) "move left more")
+  ("C-S-j" (lambda () (interactive) (rigpa-char-move-down 'more)) "move down more")
+  ("C-S-k" (lambda () (interactive) (rigpa-char-move-up 'more)) "move up more")
+  ("C-S-l" (lambda () (interactive) (rigpa-char-move-right 'more)) "move right more")
+  ("M-H" (lambda () (interactive) (rigpa-char-move-left 'most)) "move to far left")
+  ("M-J" (lambda () (interactive) (rigpa-char-move-down 'most)) "move to bottom")
+  ("M-K" (lambda () (interactive) (rigpa-char-move-up 'most)) "move to top")
+  ("M-L" (lambda () (interactive) (rigpa-char-move-right 'most)) "move to far right")
+  ("c" rigpa-char-change "change" :exit t)
+  ("y" rigpa-char-yank "yank (copy)" :exit t)
+  ("x" rigpa-char-delete "delete")
+  ("~" rigpa-char-toggle-case "toggle case")
+  ("i" rigpa-char-info "info" :exit t)
+  ("?" rigpa-char-info "info" :exit t)
   ("H-m" rigpa-toggle-menu "show/hide this menu")
   ("<return>" rigpa-enter-lower-level "enter lower level" :exit t)
   ("<escape>" rigpa-enter-higher-level "escape to higher level" :exit t))
