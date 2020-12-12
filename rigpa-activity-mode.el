232978e7ec937dda82bbf71e1cd4751fb5e3444e
Siddhartha <sid@countvajhula.com>
2020-12-12 10:29:54 -0800

    rename activity mode interfaces

--- rigpa-activity-mode.el
+++ rigpa-activity-mode.el
@@ -7,33 +7,33 @@
   :message "-- ACTIVITY --"
   :enable (normal))
 
-(setq my-accumulate-buffer-name "MY-CLIPBOARD")
+(setq rigpa-activity-accumulate-buffer-name "MY-CLIPBOARD")
 
-(defun my-yank-and-accumulate ()
+(defun rigpa-activity-yank-and-accumulate ()
   "'Yank'/accumulate text in a temporary buffer."
   (interactive)
-  (unless (get-buffer my-accumulate-buffer-name)
-    (my-new-empty-buffer my-accumulate-buffer-name))
-  (append-to-buffer my-accumulate-buffer-name
+  (unless (get-buffer rigpa-activity-accumulate-buffer-name)
+    (rigpa-buffer-create rigpa-activity-accumulate-buffer-name))
+  (append-to-buffer rigpa-activity-accumulate-buffer-name
                     (region-beginning)
                     (region-end))
   (deactivate-mark))
 
-(defun my-paste-and-clear ()
+(defun rigpa-activity-paste-and-clear ()
   "Paste contents of paste buffer and empty it."
   (interactive)
-  (insert-buffer my-accumulate-buffer-name)
-  (kill-buffer my-accumulate-buffer-name))
+  (insert-buffer rigpa-activity-accumulate-buffer-name)
+  (kill-buffer rigpa-activity-accumulate-buffer-name))
 
-(defun my-goto-older-change ()
+(defun rigpa-activity-previous ()
   "docstring"
   (interactive)
   (call-interactively 'goto-last-change))
 
-(defun my-goto-newer-change ()
+(defun rigpa-activity-next ()
   "docstring"
   (interactive)
-  (when (eq last-command 'my-goto-older-change)
+  (when (eq last-command 'rigpa-activity-previous)
     ;; emacs only cycles "forwards" through the change list
     ;; if the previous command was a "backwards" navigation
     ;; through this list. Since we're wrapping the internal
@@ -49,16 +49,16 @@
                           :after-exit (chimera-hydra-signal-exit chimera-activity-mode
                                                                  #'chimera-handle-hydra-exit))
   "Activity mode"
-  ("h" my-goto-older-change "previous change in buffer")
+  ("h" rigpa-activity-previous "previous change in buffer")
   ("C-j" evil-jump-backward "jump backward") ;; TODO: these jumps don't work via hydra atm
   ("C-k" evil-jump-forward "jump forward")
-  ("l" my-goto-newer-change "next change in buffer")
+  ("l" rigpa-activity-next "next change in buffer")
   ("m" evil-set-marker "set mark")
   ("g" evil-goto-mark "go to mark")
-  ("y" my-yank-and-accumulate "yank and accumulate") ;; TODO: these don't work via hydra atm
-  ("p" my-paste-and-clear "paste and clear")
-  ("a" my-goto-older-change "previous change in buffer" :exit t)
-  ("s-a" my-goto-older-change "previous change in buffer" :exit t)
+  ("y" rigpa-activity-yank-and-accumulate "yank and accumulate") ;; TODO: these don't work via hydra atm
+  ("p" rigpa-activity-paste-and-clear "paste and clear")
+  ("a" rigpa-activity-previous "previous change in buffer" :exit t)
+  ("s-a" rigpa-activity-previous "previous change in buffer" :exit t)
   ("i" ignore "exit" :exit t)
   ("H-m" rigpa-toggle-menu "show/hide this menu")
   ("<return>" rigpa-enter-lower-level "enter lower level" :exit t)
