d128bcca1b53ff8e41893e41564c43c8c63ba002
Siddhartha <sid@countvajhula.com>
2024-03-27 17:06:46 -0700

    Overload Esc to rotate ring in non-insertion states
    This is just a quick hack to get the desired behavior. We'll revisit
    it down the line.

--- rigpa.el
+++ rigpa.el
@@ -224,8 +224,9 @@
     (let ((keymap (symbol-value
                    (intern
                     (concat "evil-" state "-state-map")))))
-      (define-key keymap [escape] #'rigpa-enter-higher-level)
-      (unless (member state chimera-insertion-states)
+      (if (member state chimera-insertion-states)
+          (define-key keymap [escape] #'rigpa-enter-higher-level)
+        (define-key keymap [escape] #'rigpa-rotate-mode-ring-left)
         (define-key keymap [return] #'rigpa--enter-lower-or-pass-through))))
   ;; exit visual state gracefully
   (define-key evil-visual-state-map [escape] (lambda ()
