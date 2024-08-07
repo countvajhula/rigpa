b23e09c630c7a69a09e5fc9f98ad5ab225471e6f
Siddhartha <sid@countvajhula.com>
2024-08-07 12:49:41 -0700

    Migrate Activity mode from Hydra → Lithium

--- rigpa-activity-mode.el
+++ rigpa-activity-mode.el
@@ -27,9 +27,8 @@
 ;;; Code:
 
 (require 'evil)
-(require 'hydra)
+(require 'lithium)
 (require 'chimera)
-(require 'chimera-hydra)
 
 (evil-define-state activity
   "Activity state."
@@ -89,41 +88,64 @@
     (setq last-command 'goto-last-change))
   (call-interactively 'goto-last-change-reverse))
 
-
-(defhydra hydra-activity (:columns 2
-                          :body-pre (chimera-hydra-signal-entry chimera-activity-mode)
-                          :post (chimera-hydra-portend-exit chimera-activity-mode t)
-                          :after-exit (chimera-hydra-signal-exit chimera-activity-mode
-                                                                 #'chimera-handle-hydra-exit))
+(lithium-define-mode rigpa-activity-mode
   "Activity mode"
-  ("h" rigpa-activity-previous "previous change in buffer")
-  ("C-j" evil-jump-backward "jump backward") ;; TODO: these jumps don't work via hydra atm
-  ("C-k" evil-jump-forward "jump forward")
-  ("l" rigpa-activity-next "next change in buffer")
-  ("m" evil-set-marker "set mark")
-  ("g" evil-goto-mark "go to mark")
-  ("y" rigpa-activity-yank-and-accumulate "yank and accumulate") ;; TODO: these don't work via hydra atm
-  ("p" rigpa-activity-paste-and-clear "paste and clear")
-  ("a" rigpa-activity-previous "previous change in buffer" :exit t)
-  ("s-a" rigpa-activity-previous "previous change in buffer" :exit t)
-  ("i" ignore "exit" :exit t)
-  ("H-m" rigpa-toggle-menu "show/hide this menu")
-  ("<return>" rigpa-enter-lower-level "enter lower level" :exit t)
-  ("<escape>" rigpa-enter-higher-level "escape to higher level" :exit t))
+  (("h" rigpa-activity-previous)
+   ("C-j" evil-jump-backward)
+   ("C-k" evil-jump-forward)
+   ("l" rigpa-activity-next)
+   ("m" evil-set-marker)
+   ("g" evil-goto-mark)
+   ("y" rigpa-activity-yank-and-accumulate)
+   ("p" rigpa-activity-paste-and-clear)
+   ("a" rigpa-activity-previous t)
+   ("s-a" rigpa-activity-previous t)
+   ("i" ignore t)
+   ("H-m" rigpa-toggle-menu)
+   ("<return>" rigpa-enter-lower-level t)
+   ("<escape>" rigpa-enter-higher-level t))
+  :lighter " activity"
+  :group 'rigpa)
 
-(defvar chimera-activity-mode-entry-hook nil
-  "Entry hook for rigpa activity mode.")
+(defun rigpa--on-activity-mode-entry ()
+  "Actions to take upon entering activity mode."
+  (evil-activity-state))
 
-(defvar chimera-activity-mode-exit-hook nil
-  "Exit hook for rigpa activity mode.")
+(defun rigpa--on-activity-mode-post-exit ()
+  "Actions to take upon exiting activity mode."
+  (rigpa--enter-appropriate-mode))
+
+(defun rigpa-enter-activity-mode ()
+  "Enter activity mode.
+
+We would prefer to have a thunk here so it's more easily usable with
+hooks than anonymous lambdas. The minor mode function called without
+arguments toggles rather than enters or exits, so this is more
+explicit.
+
+TODO: generate this and `exit' in the lithium mode-defining macro."
+  (lithium-enter-mode 'rigpa-activity-mode))
+
+(defun rigpa-exit-activity-mode ()
+  "Exit activity mode.
+
+We would prefer to have a thunk here so it's more easily usable with
+hooks than anonymous lambdas. The minor mode function called without
+arguments toggles rather than enters or exits, so this is more
+explicit.
+
+TODO: generate this and `enter' in the lithium mode-defining macro."
+  (lithium-exit-mode 'rigpa-activity-mode))
 
 (defvar chimera-activity-mode
   (make-chimera-mode :name "activity"
-                     :enter #'hydra-activity/body
-                     :pre-entry-hook 'chimera-activity-mode-entry-hook
-                     :post-exit-hook 'chimera-activity-mode-exit-hook
-                     :entry-hook 'evil-activity-state-entry-hook
-                     :exit-hook 'evil-activity-state-exit-hook))
+                     :enter #'rigpa-enter-activity-mode
+                     :exit #'rigpa-exit-activity-mode
+                     :pre-entry-hook 'rigpa-activity-mode-pre-entry-hook
+                     :post-exit-hook 'rigpa-activity-mode-post-exit-hook
+                     :entry-hook 'rigpa-activity-mode-post-entry-hook
+                     :exit-hook 'rigpa-activity-mode-pre-exit-hook
+                     :manage-hooks nil))
 
 
 (provide 'rigpa-activity-mode)
