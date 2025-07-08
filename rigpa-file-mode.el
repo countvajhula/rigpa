ee1e83c3730ee78ab5082cea3d45967548659992
Siddhartha <sid@countvajhula.com>
2025-07-07 19:26:10 -0700

    fix indent

--- rigpa-file-mode.el
+++ rigpa-file-mode.el
@@ -32,16 +32,16 @@
 
 ;; From: https://www.emacswiki.org/emacs/MarkCommands#toc4
 (defun unpop-to-mark-command ()
-    "Unpop off mark ring. Does nothing if mark ring is empty."
-    (interactive)
-    (when mark-ring
-      (let ((pos (marker-position (car (last mark-ring)))))
-        (if (not (= (point) pos))
-            (goto-char pos)
-          (setq mark-ring (cons (copy-marker (mark-marker)) mark-ring))
-          (set-marker (mark-marker) pos)
-          (setq mark-ring (nbutlast mark-ring))
-          (goto-char (marker-position (car (last mark-ring))))))))
+  "Unpop off mark ring. Does nothing if mark ring is empty."
+  (interactive)
+  (when mark-ring
+    (let ((pos (marker-position (car (last mark-ring)))))
+      (if (not (= (point) pos))
+          (goto-char pos)
+        (setq mark-ring (cons (copy-marker (mark-marker)) mark-ring))
+        (set-marker (mark-marker) pos)
+        (setq mark-ring (nbutlast mark-ring))
+        (goto-char (marker-position (car (last mark-ring))))))))
 
 (defun xah-pop-local-mark-ring ()
   "Move cursor to last mark position of current buffer.
