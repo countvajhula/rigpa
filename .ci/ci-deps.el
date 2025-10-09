075facb22e5c60bf94f2931da84fe10acf33d4ed
Siddhartha <sid@countvajhula.com>
2025-10-08 23:13:09 -0700

    ci: use new approach of defining recipes

--- .ci/ci-deps.el
+++
@@ -1,3 +0,0 @@
-;; This project has no external dependencies that need to be explicitly
-;; defined, as they are all correctly discovered from Package-Requires.
-(provide 'ci-deps)
---
+++ .ci/recipes.el
@@ -0,0 +1,1 @@
+((rigpa :type git :local-repo "." :files ("*.el")))
